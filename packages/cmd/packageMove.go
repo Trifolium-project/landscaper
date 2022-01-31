adfb57b313bd24326939136ec3612afac439b86f
Aleksandr Ivanov <shamrockspb@gmail.com>
2022-01-31 22:47:11 +0300

    Move package and deploy iflows

--- packages/cmd/packageMove.go
+++ packages/cmd/packageMove.go
@@ -17,9 +17,16 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
 
+	"github.com/Trifolium-project/landscaper/packages/cpiclient"
 	"github.com/spf13/cobra"
 )
+
+var targetEnv *string
+var toDeploy *bool
 
 // moveCmd represents the move command
 var packageMoveCmd = &cobra.Command{
@@ -32,7 +39,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("package move called")
+		packageMove()
 	},
 }
 
@@ -40,7 +47,8 @@ func init() {
 	packageCmd.AddCommand(packageMoveCmd)
 
 	// Here you will define your flags and configuration settings.
-
+	targetEnv = packageMoveCmd.Flags().String("target-env", "", "Target environment")
+	toDeploy = packageMoveCmd.Flags().BoolP("deploy", "d", false , "Indicate whether necessary to deploy changed artifacts in target environment")
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// moveCmd.PersistentFlags().String("foo", "", "A help for foo")
@@ -48,4 +56,93 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// moveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func packageMove() {
+
+	if globalLandscape == nil {
+		println("Global landscape is not instantiated")
+		return
+	}
+
+	originalEnvironment := globalLandscape.OriginalEnvironment
+
+	targetEnvironment, err := globalLandscape.GetEnvironment(*targetEnv)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	targetPackageId := *pkg + targetEnvironment.Suffix
+
+	
+
+	sourcePackage, err := originalEnvironment.System.Client.ReadIntegrationPackage(*pkg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fetchArtifactConfig := true
+
+	artifacts, err := originalEnvironment.System.Client.ReadIntegrationDesigntimeArtifacts(*pkg, fetchArtifactConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	
+
+	tagretPackage, err := targetEnvironment.System.Client.ReadIntegrationPackage(targetPackageId)
+	if err != nil {
+		log.Println(err)
+	} else {
+		println(tagretPackage.Id)
+	}
+
+	if tagretPackage == nil {
+
+		tagretPackage := &cpiclient.IntegrationPackage{
+			Id:          targetPackageId,
+			Name:        targetEnvironment.Suffix + " " + sourcePackage.Name,
+			Description: sourcePackage.Description,
+			ShortText:   sourcePackage.ShortText + "(environment - '" + targetEnvironment.Suffix + "')",
+
+			Keywords:    "",
+		}
+
+		err = targetEnvironment.System.Client.CreateIntegrationPackage(tagretPackage)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+	}
+
+	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
+	fmt.Fprintln(writer, "#\tArtefactId\tVersion\tPackage")
+
+	for index, art := range artifacts {
+
+		newArtifact, err := originalEnvironment.System.Client.DownloadIntegrationDesigntimeArtifact(art.Id, art.Version)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		newArtifact.PackageId = targetPackageId
+		newArtifact.Id = art.Id + targetEnvironment.Suffix
+		newArtifact.Description = art.Description + " " + targetEnvironment.Suffix
+		
+		err = targetEnvironment.System.Client.UploadIntegrationDesigntimeArtifact(newArtifact)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if *toDeploy {
+			err = targetEnvironment.System.Client.DeployIntegrationDesigntimeArtifact(newArtifact.Id, newArtifact.Version)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+		fmt.Fprintf(writer, "%d\t%s\t%s\t%s\n", index, art.Id, art.Version, art.PackageId)
+		//fmt.Fprintf(writer, "%d\t%s\t%s\n", index, pkg.Id, pkg.Name)
+	}
+	writer.Flush()
 }
